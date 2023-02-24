9817cbb06a3a38cad13a52191e96c0e6361c4c1e
Gerson Rocha <gersonrocha9@gmail.com>
2023-02-24 16:18:32 -0300

    fix: fix type on user

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,8 @@
+import { User, createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 import { Button, Menu } from "../../components";
 
-import { createClient } from "@supabase/supabase-js";
 import { Pencil } from "phosphor-react";
-import { useState } from "react";
 import twitterLogo from "../../assets/logo-twitter.svg";
 import { SidebarContainer } from "./styles";
 
@@ -11,23 +11,8 @@ const supabase = createClient(
   import.meta.env.VITE_SUPABASE_KEY
 );
 
-interface UserProps {
-  id: string;
-  created_at: string;
-  last_sign_in_at: string;
-  phone: string;
-  role: string;
-  user_metadata: {
-    avatar_url: string;
-    email: string;
-    email_verified: boolean;
-    full_name: string;
-    name: string;
-  };
-}
-
 export const Sidebar = () => {
-  const [user, setUser] = useState<UserProps | null>(null);
+  const [user, setUser] = useState<User | null>(null);
 
   async function signInWithGoogle() {
     await supabase.auth.signInWithOAuth({
@@ -39,9 +24,15 @@ export const Sidebar = () => {
     await supabase.auth.signOut();
   }
 
-  supabase.auth.onAuthStateChange((event, session) => {
-    setUser(session?.user ?? null);
-  });
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      if (session?.user) {
+        setUser(session.user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
 
   return (
     <SidebarContainer>
