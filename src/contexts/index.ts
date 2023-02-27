3c540d51554772b80e6dadb9c7b7c4b642044aca
Gerson Rocha <gersonrocha9@gmail.com>
2023-02-27 11:18:42 -0300

    fix: edit context file names

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,2 +1,2 @@
-export { AuthContext, AuthProvider } from "./AuthContext";
-export { TweetContext, TweetProvider } from "./TweetContext";
+export { AuthContext, AuthProvider } from "./AuthProvider";
+export { TweetContext, TweetProvider } from "./TweetProvider";
